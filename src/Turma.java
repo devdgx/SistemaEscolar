0ca1606bbac3609773721f6ca486c7c958bb1d0f
Diego <devdgxx@gmail.com>
2026-09-14 22:56:45 -0400

    feat: add method to add students to class

---
+++ src/Turma.java
@@ -0,0 +1,60 @@
+public class Turma {
+
+    private int numeroSala;
+    private static int contadorDeTurma = 1;
+    private static int quantidadeMaxima = 20;
+    private Aluno[] alunos = new Aluno[quantidadeMaxima];
+
+
+    public Turma() {
+        this.numeroSala = contadorDeTurma;
+        contadorDeTurma++;
+
+    }
+
+
+    public boolean adicionarAlunoNaTurma(Aluno aluno) {
+        for (int i = 0; i < alunos.length; i++) {
+
+            if (alunos[i] == null) {
+                alunos[i] = aluno;
+                return true;
+
+            }
+        }
+        return false;
+    }
+
+
+    public int getNumeroSala() {
+        return numeroSala;
+    }
+
+    public void setNumeroSala(int numeroSala) {
+        this.numeroSala = numeroSala;
+    }
+
+    public static int getContadorDeTurma() {
+        return contadorDeTurma;
+    }
+
+    public static void setContadorDeTurma(int contadorDeTurma) {
+        Turma.contadorDeTurma = contadorDeTurma;
+    }
+
+    public static int getQuantidadeMaxima() {
+        return quantidadeMaxima;
+    }
+
+    public static void setQuantidadeMaxima(int quantidadeMaxima) {
+        Turma.quantidadeMaxima = quantidadeMaxima;
+    }
+
+    public Aluno[] getAlunos() {
+        return alunos;
+    }
+
+    public void setAlunos(Aluno[] alunos) {
+        this.alunos = alunos;
+    }
+}
